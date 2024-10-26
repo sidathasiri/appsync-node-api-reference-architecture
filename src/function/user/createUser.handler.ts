a1f83a39c5163c03587768a113f020996390d449
Sidath <sidathasiri@gmail.com>
2024-10-26 19:56:32 +0530

    feat(workflows): add scripts in the workflow

--- src/function/user/createUser.handler.ts
+++ src/function/user/createUser.handler.ts
@@ -1,4 +1,6 @@
 export const handler = async (event: any) => {
+  console.log("Create user request received with event:", JSON.stringify(event));
+
   return {
     success: true,
     data: {
--- src/function/user/getUser.handler.ts
+++ src/function/user/getUser.handler.ts
@@ -1,9 +1,11 @@
 export const handler = async () => {
+  console.log('Get user request received');
+  
   return {
     success: true,
     data: {
       id: "123",
-      name: "John",
+      name: "John Doe",
     },
   };
 };
