b3522d733e205f1c6063028472f1690a00efe9f7
Sidath <sidathasiri@gmail.com>
2024-10-26 21:44:36 +0530

    feat(lambda authorizer): add lambda authorizer infra

--- lib/appsync.ts
+++ lib/appsync.ts
@@ -1,19 +1,22 @@
-import { Construct } from "constructs";
+import { Construct } from 'constructs';
 import {
   AuthorizationType,
   Definition,
   GraphqlApi,
-} from "aws-cdk-lib/aws-appsync";
-import path = require("path");
+} from 'aws-cdk-lib/aws-appsync';
+import path = require('path');
 import {
   Effect,
   PolicyDocument,
   PolicyStatement,
   Role,
   ServicePrincipal,
-} from "aws-cdk-lib/aws-iam";
-import { CreateUserResolver } from "./resolvers/createUserResolver";
-import { GetUserResolver } from "./resolvers/getUserResolver";
+} from 'aws-cdk-lib/aws-iam';
+import { CreateUserResolver } from './resolvers/createUserResolver';
+import { GetUserResolver } from './resolvers/getUserResolver';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Duration } from 'aws-cdk-lib';
 
 interface AppSyncProps {
   apiNameName: string;
@@ -25,28 +28,42 @@ export class AppSyncAPI extends Construct {
   constructor(scope: Construct, id: string, props: AppSyncProps) {
     super(scope, id);
 
-    this.appsyncAPI = new GraphqlApi(this, "Api", {
+    const authorizer = new NodejsFunction(this, 'lambda-authorizer', {
+      runtime: Runtime.NODEJS_20_X,
+      entry: path.join(
+        __dirname,
+        `../src/function/authorizer/authorizer.handler.ts`
+      ),
+      handler: 'handler',
+      functionName: 'lambda-authorizer',
+    });
+
+    this.appsyncAPI = new GraphqlApi(this, 'Api', {
       name: props.apiNameName,
       definition: Definition.fromFile(
-        path.join(__dirname, "../src/schema.graphql")
+        path.join(__dirname, '../src/schema.graphql')
       ),
       authorizationConfig: {
         defaultAuthorization: {
-          authorizationType: AuthorizationType.API_KEY,
+          authorizationType: AuthorizationType.LAMBDA,
+          lambdaAuthorizerConfig: {
+            handler: authorizer,
+            resultsCacheTtl: Duration.seconds(30),
+          },
         },
       },
     });
 
     // Create service role to AppSync to invoke lambda data sources
-    new Role(this, "appsync-lambda-invocation-role", {
-      assumedBy: new ServicePrincipal("appsync.amazonaws.com"),
+    new Role(this, 'appsync-lambda-invocation-role', {
+      assumedBy: new ServicePrincipal('appsync.amazonaws.com'),
       inlinePolicies: {
         lambdaInvoke: new PolicyDocument({
           statements: [
             new PolicyStatement({
               effect: Effect.ALLOW,
-              actions: ["lambda:invokeFunction"],
-              resources: ["*"],
+              actions: ['lambda:invokeFunction'],
+              resources: ['*'],
             }),
           ],
         }),
@@ -54,34 +71,34 @@ export class AppSyncAPI extends Construct {
     });
 
     // IAM role for lambda datasource
-    const lambdaDataSourceRole = new Role(this, "DynamoDbFullAccessRole", {
-      assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
+    const lambdaDataSourceRole = new Role(this, 'DynamoDbFullAccessRole', {
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
       managedPolicies: [
         {
           managedPolicyArn:
-            "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+            'arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole',
         },
       ],
     });
 
     lambdaDataSourceRole.addToPolicy(
       new PolicyStatement({
-        actions: ["dynamodb:*"],
-        resources: ["*"],
+        actions: ['dynamodb:*'],
+        resources: ['*'],
       })
     );
 
     // Resolver for create user mutation
-    new CreateUserResolver(this, "create-user-resolver", {
+    new CreateUserResolver(this, 'create-user-resolver', {
       appsyncAPI: this.appsyncAPI,
-      resolverName: "create-user",
+      resolverName: 'create-user',
       role: lambdaDataSourceRole,
     });
 
     // Resolver for get user query
-    new GetUserResolver(this, "get-user-resolver", {
+    new GetUserResolver(this, 'get-user-resolver', {
       appsyncAPI: this.appsyncAPI,
-      resolverName: "get-user",
+      resolverName: 'get-user',
       role: lambdaDataSourceRole,
     });
   }
